21d571fd615bdf86c6b605fcd3cc6709de73fc0e
Ferran Pedrera <ferranpedrera@gmail.com>
2021-05-30 12:41:34 +0200

    Remove index and comment Ex_17

--- Ex_17/decode_web_page.py
+++ Ex_17/decode_web_page.py
@@ -6,7 +6,6 @@ r = requests.get(url)
 r_html = r.text
 
 soup = BeautifulSoup(r_html, "html.parser")
-# print(soup.prettify()
 titles = soup.find_all(class_=["mod-title","flex-article__heading","block-list__item"])
 for title in titles:
     print("* " + title.a.get_text().strip())
